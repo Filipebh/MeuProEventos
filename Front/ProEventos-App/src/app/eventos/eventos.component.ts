167d81cc6df98288d28e1e375c8c2cf2ebe65ecb
Filipe Fernandes <filipefernandessilva@hotmail.com>
2023-04-19 22:04:33 -0300

    Seção 4: Angular By Filipe

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent {
+
+public eventos: any;
+
+constructor(private http: HttpClient) {}
+
+ngOnInit(): void{
+  this.getEventos();
+}
+
+public getEventos(): void{
+  this.http.get('http://localhost:5119/api/eventos').subscribe(
+    response => this.eventos = response,
+    error => console.log(error)
+  );
+}
+
+}
